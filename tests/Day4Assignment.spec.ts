0a5d105eba47691bd77ca01751310ca8166f8e3a
Pavithra-VAR <Pavithraraj712@gmail.com>
2025-11-29 13:45:55 +0530

    Initial Commit

---
+++ tests/Day4Assignment.spec.ts
@@ -0,0 +1,22 @@
+import { chromium, test, webkit } from "@playwright/test";
+
+test(`launch two separate browser instances using Playwright`,async ()=>{
+
+   const browser = await chromium.launch({channel:"msedge"});
+   const browserContext = await browser.newContext();
+   const page = await browserContext.newPage();
+    await page.goto(`https://www.redbus.in`);
+    console.log(page.title());
+    console.log(page.url());
+
+    const browser1 = await webkit.launch();
+   const browserContext1 = await browser1.newContext();
+   const page1 = await browserContext1.newPage();
+    await page1.goto(`https://www.flipkart.com/`);
+    await page.waitForTimeout(3000);
+   const title2= page1.title();
+   const url2= page1.url();
+    console.log(title2);
+    console.log(url2);
+
+})
---
+++ tests/Day4Assignment2.spec.ts
@@ -0,0 +1,27 @@
+import { chromium, test} from "@playwright/test";
+
+test(`Create a Lead`,async ()=>{
+
+   const browser = await chromium.launch({channel:"chrome"});
+   const browserContext = await browser.newContext();
+   const page = await browserContext.newPage();
+    await page.goto(`http://leaftaps.com/opentaps/control/main`);
+    await page.locator(`#username`).fill(`Demosalesmanager`);
+    await page.locator(`#password`).fill(`crmsfa`);
+    await page.locator(`.decorativeSubmit`).click();
+    await page.locator(`#button`).click();
+    await page.waitForTimeout(3000);
+    await page.locator(`//a[.='Leads']`).click();
+    await page.locator(`//a[.='Create Lead']`).click();
+    await page.locator(`#createLeadForm_companyName`).fill('TestLeaf')
+    await page.locator(`#createLeadForm_firstName`).fill('Pavithra')
+    await page.locator(`#createLeadForm_lastName`).fill('lastName')
+    await page.locator(`#createLeadForm_personalTitle`).fill('Creating Lead')
+    await page.locator(`#createLeadForm_generalProfTitle`).fill('Title')
+    await page.locator(`#createLeadForm_departmentName`).fill('QA department')
+    await page.locator(`#createLeadForm_annualRevenue`).fill('10000')
+    await page.locator(`#createLeadForm_primaryPhoneNumber`).fill('1234567890')
+    await page.locator(`.smallSubmit`).click();
+    await page.waitForTimeout(10000);
+
+})
